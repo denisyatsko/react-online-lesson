c1d75ac3ec8faeefb69551c8ef94edcd2ddd1a53
Denis Yatsko <yatskodi@gmail.com>
2018-12-14 14:04:40 +0200

    lesson 11 v1.1

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -2,56 +2,56 @@
 import { sum, delay, getUniqueID, getFullApiUrl } from './';
 
 describe('instruments:', () => {
-	test('sum function should be a function', () => {
-		expect(sum).toBeInstanceOf(Function);
-	});
+    test('sum function should be a function', () => {
+        expect(sum).toBeInstanceOf(Function);
+    });
 
-	test('sum function should throw, when called with non-number type as second argument', () => {
-		expect(() => sum(2, 'привет')).toThrow();
-	});
+    test('sum function should throw, when called with non-number type as second argument', () => {
+        expect(() => sum(2, 'привет')).toThrow();
+    });
 
-	test('sum function should throw, when called with non-number type as first argument', () => {
-		expect(() => sum('привет', 2)).toThrow();
-	});
+    test('sum function should throw, when called with non-number type as first argument', () => {
+        expect(() => sum('привет', 2)).toThrow();
+    });
 
-	test('delay function should return an addition of two arguments passed', () => {
-		expect(sum(3,3)).toBe(6);
-		expect(sum(1,8)).toMatchSnapshot();
-	});
+    test('delay function should return an addition of two arguments passed', () => {
+        expect(sum(3,3)).toBe(6);
+        expect(sum(1,8)).toMatchSnapshot();
+    });
 
-	test('delay function should return a resolved promise', async () => {
-		await expect(delay()).resolves.toBeUndefined();
-	});
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBeUndefined();
+    });
 
-	test('getUniqueID function should be a function', () => {
-		expect(getUniqueID).toBeInstanceOf(Function);
-	});
+    test('getUniqueID function should be a function', () => {
+        expect(getUniqueID).toBeInstanceOf(Function);
+    });
 
-	test('getUniqueID function should throw, when called with non-number type as an argument', () => {
-		expect(() => getUniqueID('привет')).toThrow();
-	});
+    test('getUniqueID function should throw, when called with non-number type as an argument', () => {
+        expect(() => getUniqueID('привет')).toThrow();
+    });
 
-	test('getUniqueID function should produce a string of a desired given length', () => {
-		expect(typeof getUniqueID()).toBe('string');
-		expect(getUniqueID(5)).toHaveLength(5);
-		expect(getUniqueID(12)).toHaveLength(12);
-	});
+    test('getUniqueID function should produce a string of a desired given length', () => {
+        expect(typeof getUniqueID()).toBe('string');
+        expect(getUniqueID(5)).toHaveLength(5);
+        expect(getUniqueID(12)).toHaveLength(12);
+    });
 
-	test('getFullApiUrl function should be a function', () => {
-		expect(getFullApiUrl).toBeInstanceOf(Function);
-	});
+    test('getFullApiUrl function should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
 
-	test('getFullApiUrl function should throw, when called with non-string type as first argument', () => {
-		expect(() => getFullApiUrl('привет', 2)).toThrow();
-	});
+    test('getFullApiUrl function should throw, when called with non-string type as first argument', () => {
+        expect(() => getFullApiUrl('привет', 2)).toThrow();
+    });
 
-	test('getFullApiUrl function should throw, when called with non-string type as second argument', () => {
-		expect(() => getFullApiUrl(2, 'привет')).toThrow();
-	});
+    test('getFullApiUrl function should throw, when called with non-string type as second argument', () => {
+        expect(() => getFullApiUrl(2, 'привет')).toThrow();
+    });
 
-	test('getFullApiUrl function should return arguments passed should be a string', () => {
-		expect(typeof getFullApiUrl('api', 'ID')).toBe('string');
-		expect(getFullApiUrl('api', 'ID')).toMatchSnapshot();
-	});
+    test('getFullApiUrl function should return arguments passed should be a string', () => {
+        expect(typeof getFullApiUrl('api', 'ID')).toBe('string');
+        expect(getFullApiUrl('api', 'ID')).toMatchSnapshot();
+    });
 
 });
