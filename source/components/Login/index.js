ef5605f30935de8d21318f30fd27da16e4010f7d
Denis Yatsko <yatskodi@gmail.com>
2018-12-27 17:47:01 +0200

    lesson 13 v1.1

--- source/components/Login/index.js
+++ source/components/Login/index.js
@@ -11,22 +11,22 @@ import { Consumer, withProfile } from 'components/HOC/withProfile';
 
 @withRouter
 export default class Login extends Component {
-	static propTypes = {
+    static propTypes = {
         _setloginState: func.isRequired,
-    }	
+    }   
 
     _authButton = () => {
-    	this.props.history.push('/feed');
-    	
-    	this.props._setloginState(true);
+        this.props.history.push('/feed');
+        
+        this.props._setloginState(true);
     }
 
-	render() {
-		return (
+    render() {
+        return (
             <section className = { Styles.login }>
                 <h1>Please, click to log in</h1>
                 <button onClick = { this._authButton }>Log In</button>
             </section>
         );
-	}
+    }
 }
